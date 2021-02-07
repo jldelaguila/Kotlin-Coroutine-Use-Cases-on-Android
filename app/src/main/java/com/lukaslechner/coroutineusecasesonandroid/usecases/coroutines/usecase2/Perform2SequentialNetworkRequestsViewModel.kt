a3deaa5f214b0615860c0b65217e72cdabfc6717
Jorge Del Aguila <jorgeluisdelaguilacardenas@Jorges-MacBook-Pro.local>
2021-02-07 15:03:47 -0500

    Exercise 1 solution + removing .idea folder from remote

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
@@ -1,14 +1,25 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase2
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
 
 class Perform2SequentialNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun perform2SequentialNetworkRequest() {
-        // TODO: Exercise 1
-        // switch to branch "coroutine_course_full" to see solution
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            try {
+                val versions = mockApi.getRecentAndroidVersions()
+                val lastVersion = versions.last()
+                val features = mockApi.getAndroidVersionFeatures(lastVersion.apiLevel)
+                uiState.value = UiState.Success(features)
+            } catch (e: Exception) {
+                uiState.value = UiState.Error("Something went wrong!")
+            }
+        }
     }
 }
