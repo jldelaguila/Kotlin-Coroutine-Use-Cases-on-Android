f401180f0d7738a9aa0a44a5b4145095337048f6
Jorge Del Aguila <jorge.delaguila@pucp.pe>
2021-02-27 15:35:24 -0500

    Exercise 5 solution

--- app/src/test/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModelTest.kt
+++ app/src/test/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModelTest.kt
@@ -1,8 +1,10 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase2
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.lukaslechner.coroutineusecasesonandroid.mock.mockVersionFeaturesAndroid10
 import com.lukaslechner.coroutineusecasesonandroid.utils.MainCoroutineScopeRule
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
@@ -20,17 +22,59 @@ class Perform2SequentialNetworkRequestsViewModelTest {
 
     @Test
     fun `should return Success when both network requests are successful`() {
-        // TODO
+        // Arrange
+        val viewModel = Perform2SequentialNetworkRequestsViewModel(FakeSuccessApi())
+        viewModel.observe()
+
+        // Act
+        viewModel.perform2SequentialNetworkRequest()
+
+        // Assert
+        assertEquals(
+            listOf(
+                UiState.Loading,
+                UiState.Success(mockVersionFeaturesAndroid10)
+            ),
+            receivedUiStates
+        )
     }
 
     @Test
     fun `should return Error when first network requests fails`() {
-        // TODO
+        // Arrange
+        val viewModel = Perform2SequentialNetworkRequestsViewModel(FakeVersionsErrorApi())
+        viewModel.observe()
+
+        // Act
+        viewModel.perform2SequentialNetworkRequest()
+
+        // Assert
+        assertEquals(
+            listOf(
+                UiState.Loading,
+                UiState.Error("Something went wrong!")
+            ),
+            receivedUiStates
+        )
     }
 
     @Test
     fun `should return Error when second network requests fails`() {
-        // TODO
+        // Arrange
+        val viewModel = Perform2SequentialNetworkRequestsViewModel(FakeFeaturesErrorApi())
+        viewModel.observe()
+
+        // Act
+        viewModel.perform2SequentialNetworkRequest()
+
+        // Assert
+        assertEquals(
+            listOf(
+                UiState.Loading,
+                UiState.Error("Something went wrong!")
+            ),
+            receivedUiStates
+        )
     }
 
     private fun Perform2SequentialNetworkRequestsViewModel.observe() {
